bd7d4d91d509896fb9e7b902ff22cd71adec54e6
sorafka <weekinweekout@gmail.com>
2015-03-06 11:11:10 +0900

    spec1: オムライス出力機能を実装

---
+++ src/com/weekinweekout/RecipeManegement.java
@@ -0,0 +1,9 @@
+package com.weekinweekout;
+
+public class RecipeManegement {
+	static String OMELET_RICE = "ƒIƒ€ƒ‰ƒCƒX";
+
+	public static void main(String[] args) {
+		System.out.println(OMELET_RICE);
+	}
+}
